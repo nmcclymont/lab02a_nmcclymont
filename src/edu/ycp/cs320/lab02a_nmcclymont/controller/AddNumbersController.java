9c693efb5a54098535f410161c35a40a72916e55
nmcclymont <125151029+nmcclymont@users.noreply.github.com>
2023-02-17 22:29:24 -0500

    Delete AddNumbersController.java

--- src/edu/ycp/cs320/lab02a_nmcclymont/controller/AddNumbersController.java
+++
@@ -1,7 +0,0 @@
-package edu.ycp.cs320.lab02a_nmcclymont.controller;
-
-public class AddNumbersController {
-	public Double add(Double first, Double second, Double third) {
-		return first + second + third;
-	}
-}

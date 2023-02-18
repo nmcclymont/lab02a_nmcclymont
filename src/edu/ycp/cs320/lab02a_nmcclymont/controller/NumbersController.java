7691c6de77882daa32689b193abcd66f68867e55
nmcclymont <125151029+nmcclymont@users.noreply.github.com>
2023-02-17 22:28:47 -0500

    numbersController name change

---
+++ src/edu/ycp/cs320/lab02a_nmcclymont/controller/NumbersController.java
@@ -0,0 +1,7 @@
+package edu.ycp.cs320.lab02a_nmcclymont.controller;
+
+public class NumbersController {
+	public Double add(Double first, Double second, Double third) {
+		return first + second + third;
+	}
+}
--- src/edu/ycp/cs320/lab02a_nmcclymont/servlet/AddNumbersServlet.java
+++ src/edu/ycp/cs320/lab02a_nmcclymont/servlet/AddNumbersServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import edu.ycp.cs320.lab02a_nmcclymont.controller.AddNumbersController;
+import edu.ycp.cs320.lab02a_nmcclymont.controller.NumbersController;
 
 public class AddNumbersServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -50,7 +50,7 @@ public class AddNumbersServlet extends HttpServlet {
 			// the view does not alter data, only controller methods should be used for that
 			// thus, always call a controller method to operate on the data
 			else {
-				AddNumbersController controller = new AddNumbersController();
+				NumbersController controller = new NumbersController();
 				result = controller.add(first, second, third);
 			}
 		} catch (NumberFormatException e) {
